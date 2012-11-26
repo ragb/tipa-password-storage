08092298b7bc5a5f2cb216a6edaeeb9be49cd091
Rui Batista <ruiandrebatista@gmail.com>
2012-11-26 12:41:14 +0000

    Minor typo and corrections.

--- src/main/java/net/tiflotecnia/tipa/components/passwordStorage/KeyDerivator.java
+++ src/main/java/net/tiflotecnia/tipa/components/passwordStorage/KeyDerivator.java
@@ -12,14 +12,16 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 /**
- * Class to derive keys from passwords, using various algorithms; keys are suitable for password storage.
+ * Class to derive keys from passwords, using various algorithms; keys are
+ * suitable for password storage.
  * <p>
- * This class uses typesafe configuration for it's parameterization.
- * All keys are kept on the net.tiflotecnia.components.passwordStorage config subtree.
- * For the used parameters and format please check the reference configuration file in
- * src/main/resources/reference.conf
+ * This class uses typesafe configuration for it's parameterization. All keys
+ * are kept on the net.tiflotecnia.components.passwordStorage config subtree.
+ * For the used parameters and format please check the reference configuration
+ * file in src/main/resources/reference.conf
  * <p>
- * With the algorithms subkey, we can add external algorithms.
+ * With the algorithms subkey, we can add more algorithms, even external from
+ * this package. See the bundled sscrypt and plain examples.
  * 
  * @author Rui Batista
  * @see Config
@@ -56,30 +58,35 @@ public class KeyDerivator {
 	}
 
 	/**
-	 * Derives  a key from the given password, using the given algorithm
-	 * @param password the password
-	 * @param algorithm the algorithm to use (algorithm identifier)
+	 * Derives a key from the given password, using the given algorithm
+	 * 
+	 * @param password
+	 *            the password
+	 * @param algorithm
+	 *            the algorithm to use (algorithm identifier)
 	 * @return the new key containing algorithm identification
 	 */
 	public String deriveKey(String password, String algorithm) {
 		AbstractKeyDerivationAlgorithm instance = getAlgorithm(algorithm);
 		return algorithm + ':' + instance.generateKey(password);
 	}
-	
+
 	/**
 	 * Derives a key from the given password, using the default algorithm.
-	 * @param password the password
+	 * 
+	 * @param password
+	 *            the password
 	 * @return the derived key.
 	 */
 	public String deriveKey(String password) {
 		return deriveKey(password, _defaultAlgorithm);
 	}
-	
+
 	public boolean check(String password, String key) {
 		int index = key.indexOf(':');
-		String algorithm = key.substring(0,  index);
+		String algorithm = key.substring(0, index);
 		AbstractKeyDerivationAlgorithm instance = getAlgorithm(algorithm);
-		return instance.check(password, key.substring(index+1));	
+		return instance.check(password, key.substring(index + 1));
 	}
 
 	public final Set<String> getAvailableAlgorithms() {
@@ -87,29 +94,32 @@ public class KeyDerivator {
 	}
 
 	private void populateAlgorithms() {
-	for(Config c : _config.getConfigList("algorithms")) {
-String identifier = c.getString("identifier");
-Config params = c.getConfig("params");
-String className = c.getString("class");
-AbstractKeyDerivationAlgorithm algorithm;
-try {
-	Class<?> classRef = Class.forName(className);
-	algorithm = (AbstractKeyDerivationAlgorithm)classRef.getConstructor(Config.class).newInstance(params);
-} catch (ClassNotFoundException | InstantiationException
-		| IllegalAccessException | IllegalArgumentException
-		| InvocationTargetException | NoSuchMethodException | SecurityException e) {
-// This is to be used on a non-exception trappy context.
-	throw new RuntimeException("Error creating key derivation algorithm class", e);
+		for (Config c : _config.getConfigList("algorithms")) {
+			String identifier = c.getString("identifier");
+			Config params = c.getConfig("params");
+			String className = c.getString("class");
+			AbstractKeyDerivationAlgorithm algorithm;
+			try {
+				Class<?> classRef = Class.forName(className);
+				algorithm = (AbstractKeyDerivationAlgorithm) classRef
+						.getConstructor(Config.class).newInstance(params);
+			} catch (ClassNotFoundException | InstantiationException
+					| IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException | NoSuchMethodException
+					| SecurityException e) {
+				// This is to be used on a non-exception trappy context.
+				throw new RuntimeException(
+						"Error creating key derivation algorithm class", e);
 
-}
-_algorithms.put(identifier, algorithm);
+			}
+			_algorithms.put(identifier, algorithm);
+		}
 	}
-}
-	
+
 	private AbstractKeyDerivationAlgorithm getAlgorithm(String algorithm) {
 		if (!_algorithms.containsKey(algorithm)) {
-			throw new IllegalArgumentException("Unkown algorithm: " + algorithm);
+			throw new IllegalArgumentException("Unknown algorithm: " + algorithm);
 		}
-return _algorithms.get(algorithm);
+		return _algorithms.get(algorithm);
 	}
 }
